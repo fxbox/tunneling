cf60519a3e84eb5a67f0d07d1b977078bfd04cfb
Francisco Jordano <fjordano@mozilla.com>
2016-02-16 15:07:03 +0000

    Hide the output from the different commands and just explain what's happening

--- custom-ngrok-build/generate.sh
+++ custom-ngrok-build/generate.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
 
 # Clean up
+echo "Cleaning up ..."
 rm -rf ngrok
 rm -rf client
 rm -rf server
 
 DOMAIN="${NGROK_DOMAIN:=example.com}"
-git clone https://github.com/inconshreveable/ngrok.git
+echo "Building custom ngrok for domain $DOMAIN"
+
+echo "Cloning ngrok repo"
+git clone https://github.com/inconshreveable/ngrok.git > /dev/null 2>&1
 cd ngrok
 
 #Apply patches
-wget https://patch-diff.githubusercontent.com/raw/inconshreveable/ngrok/pull/308.diff
-git apply 308.diff
+echo "Patching ngrok"
+# This first patch is to change the log4go path
+wget https://patch-diff.githubusercontent.com/raw/inconshreveable/ngrok/pull/308.diff > /dev/null 2>&1
+git apply 308.diff > /dev/null 2>&1
 
-openssl genrsa -out rootCA.key 2048
-openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$DOMAIN" -days 5000 -out rootCA.pem
-openssl genrsa -out device.key 2048
-openssl req -new -key device.key -subj "/CN=$DOMAIN" -out device.csr
-openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000
+#Create certificates
+echo "Creating custom certificates for $DOMAIN"
+openssl genrsa -out rootCA.key 2048 > /dev/null 2>&1
+openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$DOMAIN" -days 5000 -out rootCA.pem > /dev/null 2>&1
+openssl genrsa -out device.key 2048 > /dev/null 2>&1
+openssl req -new -key device.key -subj "/CN=$DOMAIN" -out device.csr > /dev/null 2>&1
+openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000 > /dev/null 2>&1
 
-cp rootCA.pem assets/client/tls/ngrokroot.crt
-make clean
+cp rootCA.pem assets/client/tls/ngrokroot.crt > /dev/null 2>&1
+make clean > /dev/null 2>&1
 
 # The server is always built for a linux target
-GOOS="linux" GOARCH="amd64" make release-server
+echo "Building server ..."
+GOOS="linux" GOARCH="amd64" make release-server > /dev/null 2>&1
 
 # Check if we were asked to build an specific OS via $TUNNEL_TARGET
 # otherwise detect from the current one
 GOOS="darwin"
 if [ -z "$TUNNEL_TARGET" ]; then
-  GOOS=$TUNNEL_TARGET
+  GOOS="$TUNNEL_TARGET"
 else
   platform='unknown'
   unamestr=`uname`
@@ -38,14 +47,15 @@ else
   fi
 fi
 
-# Build the client with arm64 architecture unless overwritten
-# in $TUNNEL_ARCH
+# Build the client with amd64 architecture unless overwritten
+# in $TUNNEL_ARCH (for rpi use TUNNEL_ARCH=arm)
 GOARCH="amd64"
 if [ -z "$TUNNEL_ARCH"]; then
   GOARCH="$TUNNEL_ARCH"
 fi
 
-GOOS="$GOOS" GOARCH="$GOARCH" make release-client
+echo "Building client $GOOS for $GOARCH ..."
+GOOS="$GOOS" GOARCH="$GOARCH" make release-client > /dev/null 2>&1
 
 # Provide server files and client files in separate folders
 cd ..
@@ -62,3 +72,5 @@ cp ngrok/device.crt server
 cp ngrok/bin/linux_amd64/ngrokd server
 echo -e "./ngrokd -tlsKey=device.key -tlsCrt=device.crt -domain=\"$DOMAIN\"" > server/launch.sh
 chmod +x server/launch.sh
+
+echo "Job finished, look for your client and server folders!"
