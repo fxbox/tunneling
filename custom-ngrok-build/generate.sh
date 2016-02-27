5e47609c4eb4fab8fefe4ea5052caba46d991760
root <root@jano.(none)>
2016-02-28 01:06:16 +0300

    Detecting dependencies

--- custom-ngrok-build/generate.sh
+++ custom-ngrok-build/generate.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Checking for Go
+echo "Checking dependencies ..."
+if hash go 2>/dev/null; then
+  echo "Found GO!"
+else
+  echo "Missing dependency GO compiler"
+  exit 1
+fi
+if hash go-bindata 2>/dev/null; then
+  echo "Found go-bindata"
+else
+  echo "Missing dependency go-bindata"
+  exit 2
+fi
+
 # Clean up
 echo "Cleaning up ..."
 rm -rf ngrok
@@ -16,7 +31,7 @@ cd ngrok
 #Apply patches
 echo "Patching ngrok"
 # This first patch is to change the log4go path
-wget https://patch-diff.githubusercontent.com/raw/inconshreveable/ngrok/pull/308.diff > /dev/null 2>&1
+wget --no-check-certificate https://patch-diff.githubusercontent.com/raw/inconshreveable/ngrok/pull/308.diff > /dev/null 2>&1
 git apply 308.diff > /dev/null 2>&1
 
 #Check if we have certificates from letsencrypt
@@ -26,7 +41,7 @@ echo "Checking for letsencrypt certificates"
 PRIVKEY="/etc/letsencrypt/live/${DOMAIN}/privkey.pem"
 TRUST_CERT="false"
 if [ -e "$PRIVKEY" ]; then
-  echo "LetsEncrypt certificates for ${DOMAIN} found"
+  echo "LetsEncrypt certificates for ${DOMAIN} found!"
   cp "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" assets/client/tls/ngrokroot.crt > /dev/null 2>&1
   CERTIFICATE="/etc/letsencrypt/live/${DOMAIN}/cert.pem"
   TRUST_CERT="true"
@@ -39,11 +54,9 @@ else
   openssl genrsa -out device.key 2048 > /dev/null 2>&1
   openssl req -new -key device.key -subj "/CN=$DOMAIN" -out device.csr > /dev/null 2>&1
   openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000 > /dev/null 2>&1
-
   cp rootCA.pem assets/client/tls/ngrokroot.crt > /dev/null 2>&1
   PRIVKEY="ngrok/device.key"
   CERTIFICATE="ngrok/device.crt"
-
 fi
 
 make clean > /dev/null 2>&1
@@ -87,6 +100,7 @@ EOF
 mkdir server
 cp $PRIVKEY server/device.key
 cp $CERTIFICATE server/device.crt
+cp ngrok/bin/ngrokd server
 cp ngrok/bin/linux_amd64/ngrokd server
 echo -e "./ngrokd -tlsKey=device.key -tlsCrt=device.crt -domain=\"$DOMAIN\"" > server/launch.sh
 chmod +x server/launch.sh
